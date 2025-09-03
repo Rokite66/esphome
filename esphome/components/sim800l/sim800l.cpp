fc3bef9d194fb0fc28ee9adec3d3f81433e2ca05
Rokite66 <r.scheffer@chello.nl>
2025-09-03 15:09:21 +0200

    Update sim800l.cpp

--- esphome/components/sim800l/sim800l.cpp
+++ esphome/components/sim800l/sim800l.cpp
@@ -241,6 +241,7 @@ void Sim800LComponent::parse_cmd_(std::string message) {
       this->state_ = STATE_CHECK_SMS;
       break;
     case STATE_PARSE_SMS_RESPONSE:
+      ESP_LOGV(TAG, "P: %s - %d", message.c_str(), this->parse_index_);
       if (message.compare(0, 6, "+CMGL:") == 0 && this->parse_index_ == 0) {
         size_t start = 7;
         size_t end = message.find(',', start);
